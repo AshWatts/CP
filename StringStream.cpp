5d6d8b605871a9275c9980a7e0c77a09477135e9
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-09-11 23:08:36 +0530

    Tough!

---
+++ StringStream.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the String : ";
+    cin>>str;
+    char ch;
+    string str;
+    stringstream ss(str);
+    char ch;
+    for(int i=0;i<str.length();i++)
+    {
+        ss>>i>>ch;
+    }
+    // int a, b, c;
+    // ss >> a >> ch >> b >> ch >> c;
+    // cout<<a<<" "<<b<<" "<<c;
+}
