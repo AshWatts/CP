fa4f8743b48bf16ac0d0fb059faf64e084484828
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-09-20 19:19:42 +0530

    New

--- Pointers_Basics.cpp
+++ Pointers_Basics.cpp
@@ -2,23 +2,13 @@
 using namespace std;
 
 
-
-
 int main()
 {
     int n;
-    cout<<"Enter Number of Elements : ";
+    cout<<"Enter Value : ";
     cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    int* p=a;
-    cout<<a<<endl;                      // Same as cout<<&a<<endl;
+    int *p;
     cout<<p<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cout<<p[i]<<" ";
-    }
+    cout<<&n<<endl;
+
 }
