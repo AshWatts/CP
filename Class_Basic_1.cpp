94d610f2f792230e6d07d38b516dc5781627c038
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-09-25 19:24:28 +0530

    !

--- Class_Basic_1.cpp
+++ Class_Basic_1.cpp
@@ -1,30 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class myclass
-{
+class student
+{                        // Private by Default!     
     public:
-        int age;
-        string name;
+    int age;
+    string name;
 };
 
 int main()
 {
-    myclass myobj1;
-    cout<<"Enter 1st Age : ";
-    cin>>myobj1.age;
-    cout<<"Enter 1st Name : ";
-    cin>>myobj1.name;
+    student s1;
+    cin>>s1.age;
+    cout<<"First Age : "<<s1.age<<endl;
 
-
-    myclass myobj2;
-    cout<<"Enter 2nd Age : ";
-    cin>>myobj2.age;
-    cout<<"Enter 2nd Name : ";
-    cin>>myobj2.name;
-
-    cout<<"Age : "<<myobj1.age<<" "<<myobj2.age<<endl;
-    cout<<"Name : "<<myobj1.name<<" "<<myobj2.name<<endl;
+    student s2;
+    cin>>s2.name;
+    cout<<"Second Name : "<<s2.name;
 
     return 0;
 }
