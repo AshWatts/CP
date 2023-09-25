ccbb87a1b3725b611504a3d5007ebb50a9935859
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-09-25 20:14:47 +0530

    !

--- Apple_and_Orange.cpp
+++ Apple_and_Orange.cpp
@@ -1,39 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
-    int s,t,a,b,m,n;
-    int c1=0;
-    int c2=0;
-    int apples[m];
-    int oranges[n];
-    
-    cin>>s>>t>>a>>b>>m>>n;
-    for(int i=0;i<m;i++)
-    {
-        cin>>apples[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-        cin>>oranges[i];
-    }
-    
-    for(int i=0;i<m;i++)
-    {
-        if(a+apples[i]>=s and a+apples[i]<=t)
-        {
-            c1=c1+1;
+    int s, t;
+    cin >> s >> t; //inclusive range
+    int a, b;
+    cin >> a >> b; //location of apples and oranges
+    int m, n;
+    cin >> m >> n; //number of apples and oranges
+    int arr[m];
+    int brr[n];
+    int count1 = 0;
+    int count2 = 0;
+
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+        arr[i] += a;
+        if (arr[i] >= s && arr[i] <= t) {
+            count1++;
         }
     }
-    
-    for(int i=0;i<n;i++)
-    {
-        if(b+oranges[i]<=t and b+oranges[i]>=s)
-        {
-            c2=c2+1;
+    for (int i = 0; i < n; i++) {
+        cin >> brr[i];
+        brr[i] += b;
+        if (brr[i] >= s && brr[i] <= t) {
+            count2++;
         }
     }
-    
-    cout<<c1<<endl<<c2;
+    cout << count1 << endl << count2;
+    return 0;
 }
--- Class_Basic_1.cpp
+++ Class_Basic_1.cpp
@@ -6,17 +6,27 @@ class student
     public:
     int age;
     string name;
+    
+
+    void intro()
+    {
+        cout<<"Age - "<<age<<endl;
+        cout<<"Name - "<<name<<endl<<endl;
+    }
 };
 
 int main()
 {
     student s1;
     cin>>s1.age;
-    cout<<"First Age : "<<s1.age<<endl;
+    cin>>s1.name;
 
     student s2;
+    cin>>s2.age;
     cin>>s2.name;
-    cout<<"Second Name : "<<s2.name;
+    
+    s1.intro();
+    s2.intro();
 
     return 0;
 }
