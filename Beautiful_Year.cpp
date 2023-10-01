ef4e3f07ebd4b542a5c2d9975b585ad245f6e1e3
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-10-01 17:19:46 +0530

    !

--- Beautiful_Year.cpp
+++ Beautiful_Year.cpp
@@ -10,5 +10,14 @@ int main()
         a=a+1;
         int b=a/1000;
         int c=a/100%10;
+        int d=a/10%10;
+        int e=a%10;
+
+        if(b!=c and b!=d and b!=e and c!=d and c!=e and d!=e)
+        {
+            break;
+        }
     }
+    cout<<a<<endl;
+    return 0;
 }
