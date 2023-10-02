0483fe71404bcfeec223c6f1a27179d14bf6a060
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-10-03 00:03:30 +0530

    !

--- Drinks.cpp
+++ Drinks.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hi";
-}
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    float c=0;
+    float d=0;
+    for(int i=0;i<n;i++)
+    {
+        c=c+a[i];
+        d=d+100;
+    }
+    float e;
+    e=c/d;
+    cout<<e*100;
+} 
