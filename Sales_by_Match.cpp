67c08d7f2103b0ae6967a967e9aa2c0a2bfd739c
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-10-13 19:07:25 +0530

    !

--- Sales_by_Match.cpp
+++ Sales_by_Match.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<n<<endl;
+    int m,n;
+    string a;
+    cin>>a;
+
+    for(int i=0;i<a.length();i++)
+    {
+        
+    }
 }
