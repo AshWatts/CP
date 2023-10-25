d9b749cf32085ba760592d727f59616e19b4d1ec
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-10-25 23:15:08 +0530

    !

--- Panoramixs_Prediction.cpp
+++ Panoramixs_Prediction.cpp
@@ -4,8 +4,27 @@ using namespace std;
 int main()
 {
     int n,m;
+    int count=0;
+    int c=0;
+    int k=0;
     cin>>n;
     cin>>m;
-    cout<<n+m;
-    cout<<"hi";
+
+    for(int i=2;i<m/2;i++)
+    {
+        if(m%i==0)
+        {
+            cout<<"NO";
+            count=count+1;
+            break;
+        }
+    }
+
+    int f=n;
+
+    if(count==0)
+    {
+        cout<<"YES";
+    }
+
 }
