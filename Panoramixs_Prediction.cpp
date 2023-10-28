e5a818d124a922c953a596b65eb4fef80800b5cf
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-10-28 11:27:55 +0530

    !

--- Panoramixs_Prediction.cpp
+++ Panoramixs_Prediction.cpp
@@ -1,39 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
+int checkprime(int N)
+{
+    for(int i= 2 ;i<=sqrt(N) ;i++)
+    {
+        if(N%i==0)
+        {
+            return 0; 
+        }
+    }
+    return N;
+}
 
 int main()
 {
-    int n,m;
-    int k=100;
-    cin>>n;
-    cin>>m;
-    int count=0;
+    int M ,N;
+    cin>>M>>N;
+    int idx1= -1;
+    int idx2 = -1;
+    vector<int>v;
 
-    for(int i=2;i<m/2;i++)
+for(int i =2 ;i<50 ;i++)
+{
+    if(checkprime(i))
     {
-        if(m%i==0)
-        {
-            cout<<"NO";
-            count=count+1;
-            break;
-        }
+        v.push_back(i);  //after returnning N we push that all primes in vector
     }
+}
 
-
-    if(count==0)
-    {
-        cout<<"YES";
-        for(int i=n;i<k;i++)
-        {
-            for(int j=2;j<n;j++)
-            {
-                if(i%j==0)
-                {
-                    break;
-                }
-            }
-        }
+for(int i = 0 ;i<v.size() ;i++)
+{
+    if(v[i]==M)
+    {   //iterate through a vector and get the index of M and N 
+        idx1=i;
     }
-    cout<<"HI";
-    cout<<"Uign";
+}
+
+for(int i = 0 ;i<v.size() ;i++)
+{
+    if(v[i]==N){
+        idx2=i;
+    }
+}
+
+if((idx2-idx1)==1)
+{   //if they appear one after other that is index difference is 1 then print yes otherwise no
+    cout<<"YES\n";
+}
+
+else
+{
+    cout<<"NO\n";
+}
+    return 0;
 }
