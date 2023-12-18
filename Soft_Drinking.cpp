416d91326dfe8c0357e1b51f15c1162986ddd828
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-12-18 12:50:08 +0530

    ~

---
+++ Soft_Drinking.cpp
@@ -0,0 +1,43 @@
+// This winter is so cold in Nvodsk! A group of n friends decided to buy k bottles of a soft drink called "Take-It-Light" to warm up a bit. Each bottle has l milliliters of the drink. Also they bought c limes and cut each of them into d slices. After that they found p grams of salt.
+
+//To make a toast, each friend needs nl milliliters of the drink, a slice of lime and np grams of salt. The friends want to make as many toasts as they can, provided they all drink the same amount. How many toasts can each friend make?
+
+//Input
+//The first and only line contains positive integers n, k, l, c, d, p, nl, np, not exceeding 1000 and no less than 1. The numbers are separated by exactly one space.
+
+//Output
+//Print a single integer — the number of toasts each friend can make.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,l,c,d,p,nl,np;
+    cin>>n;
+    cin>>k;
+    cin>>l;
+    cin>>c;
+    cin>>d;
+    cin>>p;
+    cin>>nl;
+    cin>>np;
+
+    int x;
+    int y;
+    int drink;
+    int lime;
+    int salt;
+
+    drink = (k*l)/nl;
+    lime = c*d;
+    salt = p/np;
+
+    x = min(drink, min(lime, salt));
+
+    y=x/n;
+
+    cout<<y;
+}
