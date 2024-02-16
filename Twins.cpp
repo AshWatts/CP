f0633c1796d5a5dec837997a55b576f76c8ad315
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-02-16 22:15:52 +0530

    `

--- Twins.cpp
+++ Twins.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() 
 {
     int n;
+    int sum1=0;
+    int sum2=0;
     cin>>n;
     int a[n];
     for(int i=0;i<n;i++)
@@ -11,8 +13,22 @@ int main()
         cin>>a[i];
     }
 
-    for(int i=0;i<n;i++)
+    sum1=a[n-1];
+    for(int i=0;i<n-1;i++)
     {
-        cout<<a[i]*a[i]<<"  ";
+        sum2+=a[i];
+    }
+
+    if(sum1>sum2)
+    {
+        cout<<1;
+    }
+    else
+    {
+        for(int i=n-2;i>=0;i--)
+        {
+            sum1+=a[i];
+            sum2-=a[i];
+        }
     }
 }
