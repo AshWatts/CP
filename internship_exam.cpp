be56e56648b23e7cebc7d83aa895fbb2c59e6502
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-04-13 23:41:37 +0530

    `

---
+++ internship_exam.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int sum=0;
+		int n;
+		cin>>n;
+		for(int i=1;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				sum+=i;
+			}
+		}
+		if(sum==n)
+		{
+			cout<<"YES"<<endl;
+		}
+		else
+		{
+			cout<<"NO"<<endl;
+		}
+	}
+}
