3aab23ca41049c5626425080a783a7952f97ade9
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-06-01 22:50:15 +0530

    `

--- Is_Anagram.cpp
+++ Is_Anagram.cpp
@@ -22,6 +22,39 @@ public:
     }
 };
 
+/* Using Hash Map
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+
+        int x=s.length();
+        unordered_map<char,int> countS;
+        unordered_map<char,int> countT;
+
+        for(int i=0;i<x;i++)
+        {
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+
+        for(int i=0;i<x;i++)
+        {
+            if(countS!=countT)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+*/
+
 int main()
 {
     Solution s;
@@ -29,4 +62,4 @@ int main()
     string s2 = "nagaram";
     cout << s.isAnagram(s1, s2) << endl;
     return 0;
-    
+}
