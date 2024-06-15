efb9407a40bb05ff83c0aabd0ff979c02c97a45e
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-06-15 11:38:18 +0530

    `

--- hcf_lcm.cpp
+++ hcf_lcm.cpp
@@ -1,45 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<long long> lcmAndGcd(long long A , long long B) {
-        long long a = min(A,B);
-        long long b = max(A,B);
-        long long lowest;
-        long long highest;
-        for(int i=a;i>0;i--){
-            if(a%i==0 && b%i==0){
-                lowest=i;
-                break;
+        long long a = A;
+        long long b = B;
+        long long lowest = 1;
+        long long highest = a*b;
+        while(A>0 && B>0){
+            if(A>B){
+                A=A%B;
             }
+            else{
+                B=B%A;
+            }
+        }
+        if(A==0){
+            lowest = B;
+        }
+        else{
+            lowest = A;
         }
         highest = (a*b)/lowest;
         return {highest,lowest};
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        long long A,B;
-        
-        cin>>A>>B;
-
-        Solution ob;
-        vector<long long> ans = ob.lcmAndGcd(A,B);
-        cout<<ans[0]<<" "<<ans[1]<<endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
 
 int main(){
     int t;
