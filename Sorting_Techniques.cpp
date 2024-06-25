47b200bcd51f7336db97b39e32a4d808cf7d79ed
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-06-25 12:57:56 +0530

    `

--- Sorting_Techniques.cpp
+++ Sorting_Techniques.cpp
@@ -63,7 +63,46 @@ class Solution
 
     // MERGE SORT
 
-    
+    void merge(int arr[], int low, int mid, int high)
+    {
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            arr[i]=temp[i-low];
+        }
+    }
+
+    void mergeSort(int arr[], int low, int high)
+    {
+        if(low==high){
+            return;
+        }
+        int mid = (low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+
+
 
     // QUICK SORT
 
@@ -99,3 +138,17 @@ class Solution
     }
 };
 
+int main(){
+    int arr[] = {5,4,1,3,2};
+    int n = 5;
+    Solution obj;
+    obj.selectionSort(arr,n);
+    obj.bubbleSort(arr,n);
+    obj.insertionSort(arr,n);
+    obj.mergeSort(arr,0,n-1);
+    obj.quickSort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
