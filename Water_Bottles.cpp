c954f52707686b255f124e40db9b758dc783b9e7
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-07-08 00:14:32 +0530

    `

---
+++ Water_Bottles.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numWaterBottles(int a, int b) {
+        int x = a;
+        while(a>=b){
+            x+=a/b;
+            a=a/b + a%b;
+        }
+        return x;
+    }
+};
+
+int main(){
+    Solution obj;
+    cout<<obj.numWaterBottles(9,3)<<endl;
+    return 0;
+}
