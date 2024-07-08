c612d10dacb2a9b01ece677f95d6dac5870216a2
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-07-08 23:42:01 +0530

    `

--- Water_Bottles.cpp
+++ Water_Bottles.cpp
@@ -15,6 +15,6 @@ public:
 
 int main(){
     Solution obj;
-    cout<<obj.numWaterBottles(9,3)<<endl;
+    cout<<obj.numWaterBottles(15,4)<<endl;
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,2 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int main()
-{
-    int n,m;
-    cin>>n;
-    cin>>m;
-    cout<<n+m;
-    cout<<n*m;
-}
+    return 0;
