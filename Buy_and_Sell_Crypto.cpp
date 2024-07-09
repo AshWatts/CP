fdfcb4c1c674025ace663e001d60f0318c1727b4
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-07-09 17:12:39 +0530

    `

--- Buy_and_Sell_Crypto.cpp
+++ Buy_and_Sell_Crypto.cpp
@@ -2,19 +2,36 @@
 #include <vector>
 using namespace std;
 
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int M=0;
+//         for(int i=0;i<prices.size();i++)
+//         {
+//             for(int j=i+1;j<prices.size();j++)
+//             {
+//                 int m=prices[j]-prices[i];
+//                 M=max(M,m);
+//             }
+//         }
+//         return M;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int M=0;
-        for(int i=0;i<prices.size();i++)
-        {
-            for(int j=i+1;j<prices.size();j++)
-            {
-                int m=prices[j]-prices[i];
-                M=max(M,m);
+        int buy = prices[0];
+        int profit = 0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            else if(prices[i]-buy>profit){
+                profit=prices[i]-buy;
             }
         }
-        return M;
+        return profit;
     }
 };
 
