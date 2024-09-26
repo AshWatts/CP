71c14eae1db3d8b95ca622850f87350224a2661e
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-09-26 10:41:42 +0530

    `

---
+++ My_Calendar_I.py
@@ -0,0 +1,34 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start: int, end: int) -> bool:
+        right = len(self.calendar)
+        if right == 0:
+            self.calendar.append((start, end))
+            return True
+
+        left = 0
+        while left < right:
+            mid = int(left + (right - left)/2)
+            if self.calendar[mid][1] <= start:
+                left = mid + 1
+            else:
+                right = mid
+
+        if left == len(self.calendar):
+            self.calendar.append((start, end))
+            return True
+                
+        if self.calendar[left][0] >= end:
+            self.calendar.insert(left, (start, end))
+            return True
+            
+        return False
+
+if __name__ == "__main__":
+    calendar = MyCalendar()
+    print(calendar.book(10, 20))  
+    print(calendar.book(15, 25))
+    print(calendar.book(20, 30)) 
