e50506421b33f1155bf822811c165b7ca0f61f30
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-09-29 13:48:23 +0530

    `

---
+++ ALL_O_one_Data_Structure.py
@@ -0,0 +1,49 @@
+class AllOne:
+    def __init__(self):
+        self.myDict = {}
+
+    def inc(self, key: str) -> None:
+        if key in self.myDict:
+            self.myDict[key] += 1
+        else:
+            self.myDict[key] = 1
+
+    def dec(self, key: str) -> None:
+        if key in self.myDict:
+            if self.myDict[key] > 1:
+                self.myDict[key] -= 1
+            else:
+                self.myDict.pop(key)
+
+    def getMaxKey(self) -> str:
+        if not self.myDict:
+            return ""
+        maxVal = max(self.myDict.values())
+        for key in self.myDict.keys():
+            if self.myDict[key] == maxVal:
+                return key
+
+    def getMinKey(self) -> str:
+        if not self.myDict:
+            return ""
+        minVal = min(self.myDict.values())
+        for key in self.myDict.keys():
+            if self.myDict[key] == minVal:
+                return key
+
+def main():
+    obj = AllOne()
+    obj.inc("apple")
+    obj.inc("banana")
+    obj.inc("apple")
+    print("Max Key:", obj.getMaxKey())  # Output: apple
+    print("Min Key:", obj.getMinKey())  # Output: banana
+    obj.dec("apple")
+    print("Max Key:", obj.getMaxKey())  # Output: apple
+    print("Min Key:", obj.getMinKey())  # Output: apple
+    obj.dec("apple")
+    print("Max Key:", obj.getMaxKey())  # Output: banana
+    print("Min Key:", obj.getMinKey())  # Output: banana
+                
+if __name__ == "__main__":
+    main()
