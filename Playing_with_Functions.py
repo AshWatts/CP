f08e49cccd5a3ace2adc251bc18f00b77ddc9802
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-10-09 17:38:21 +0530

    `

--- Playing_with_Functions.py
+++ Playing_with_Functions.py
@@ -17,3 +17,28 @@ def fib(n):
         return n
     return fib(n-1)+fib(n-2)
 print(fib(6))
+
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("Division by Zero is not allowed")
+finally:
+    print("This block will always execute")
+    
+class CustomException(Exception):
+    "Raised when the input is less than 35"
+    pass
+
+threshold = 35
+
+try:
+    marks = int(input("Enter your marks: "))
+    if marks < threshold:
+        raise CustomException("Marks are below the passing threshold.")
+    else:
+        print("You have passed the exam")
+        
+except CustomException as e:
+    print("You have failed the exam:", e)
+except ValueError:
+    print("Please enter a valid integer for marks.")
