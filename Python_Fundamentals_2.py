740c5a4fe704c66d27ee134e0fc83a05e98ab198
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-10-12 15:27:17 +0530

    `

--- Python_Fundamentals_2.py
+++ Python_Fundamentals_2.py
@@ -22,4 +22,50 @@ class class3:
     def __init__(self,name):
         self.name = name
 obj3 = class3("Ayush")
-print(obj3.name)
+print(obj3.name)    
+
+
+#Inheritance
+class Parent:
+    name = ""
+    
+    def m1(self):
+        print("Parent Class")
+        
+class Child(Parent):
+    def m2(self):
+        print("Child Class ->",self.name)
+
+obj4 = Child()
+obj4.name = "Ayush"
+obj4.m1()
+obj4.m2()
+
+
+#Lambda Function
+f = lambda x:x*x
+print(f(5))
+
+
+#Iterators
+list1 = [1,2,3,4,5]
+iter1 = iter(list1)
+print(next(iter1))
+print(next(iter1))
+
+
+#Nested Function
+def abc(x):
+    def xyz():
+        print(x*x)
+    xyz()
+abc(5)
+
+
+#Generator
+def abc():
+    for i in range(5):
+        yield i
+for i in abc():
+    print(i+1, end="")
+
