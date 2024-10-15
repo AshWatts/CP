bc8861833095f662689aab0098764660dacf4e83
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-10-15 09:39:48 +0530

    `

---
+++ Seperate_Black_and_White_Balls.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        white = 0
+        res = 0
+        j = 0
+        n = len(s)
+        while j < n:
+            if s[j] == '0':
+                res += white
+            white += 1 if s[j] == '1' else 0
+            j += 1
+        return res
+
+solution = Solution()
+print(solution.minimumSteps("001101")) 
+print(solution.minimumSteps("000111")) 
